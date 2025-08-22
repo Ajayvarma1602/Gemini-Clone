df0b380a5ed13502e3b9d25eb6a39cb038b0cdd0
T. Ajay Varma <ajayvarma1602@gmail.com>
2025-08-22 09:27:36 -0500

    Initial commit

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -25,26 +25,26 @@ const handleKeyDown = (event) => {
         {!showResult
         ?<>
           <div className="greet">
-            <p><span>Hello, Dev.</span></p>
+            <p><span>Hello, AJ.</span></p>
             <p>How can I help you today?</p>
         </div>
         <div className="cards">
         <div className="card" onClick={() => onSent("Suggest beautiful places to see on an upcoming road trip")}>
-                                        <p>Suggest beautiful places to see on an upcoming road trip</p>
-                                        <img src={assets.compass_icon} alt="compass icon" />
-                                    </div>
-                                    <div className="card" onClick={() => onSent("Briefly summarize this concept: urban planning")}>
-                                        <p>Briefly summarize this concept: urban planning</p>
-                                        <img src={assets.bulb_icon} alt="bulb icon" />
-                                    </div>
-                                    <div className="card" onClick={() => onSent("Brainstorm team bonding activities for our work retreat")}>
-                                        <p>Brainstorm team bonding activities for our work retreat</p>
-                                        <img src={assets.message_icon} alt="message icon" />
-                                    </div>
-                                    <div className="card" onClick={() => onSent("Improve the readability of the following code")}>
-                                        <p>Improve the readability of the following code</p>
-                                        <img src={assets.code_icon} alt="code icon" />
-                                    </div>
+        <p>Suggest beautiful places to see on an upcoming road trip</p>
+        <img src={assets.compass_icon} alt="compass icon" />
+        </div>
+        <div className="card" onClick={() => onSent("Briefly summarize this concept: urban planning")}>
+        <p>Briefly summarize this concept: urban planning</p>
+        <img src={assets.bulb_icon} alt="bulb icon" />
+        </div>
+        <div className="card" onClick={() => onSent("Brainstorm team bonding activities for our work retreat")}>
+        <p>Brainstorm team bonding activities for our work retreat</p>
+        <img src={assets.message_icon} alt="message icon" />
+        </div>
+        <div className="card" onClick={() => onSent("Improve the readability of the following code")}>
+        <p>Improve the readability of the following code</p>
+        <img src={assets.code_icon} alt="code icon" />
+        </div>
         </div>
         </>
         :<div className='result'>
@@ -71,6 +71,7 @@ const handleKeyDown = (event) => {
         <div className="main-bottom">
             <div className="search-box">
             <input onKeyDown={handleKeyDown} onChange={(e) => setInput(e.target.value)} value={input} type="text" placeholder="Enter a prompt here" />
+          
             <div>
                     <img src={assets.gallery_icon} alt="" /><img src={assets.mic_icon} alt="" />{input?<img onClick={()=>onSent()} src={assets.send_icon} alt="" />:null}
                 </div>
